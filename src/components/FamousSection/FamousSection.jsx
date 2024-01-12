5ee1fafb5ec43c9ace510b811e99b3f6506c73bf
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-01-11 19:33:23 -0500

    POST works. Inputs clear without breaking anything

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './FamousSection.css';
+import axios from 'axios';
 
 function FamousSection() {
   let [famousPersonName, setPersonName] = useState('');
@@ -7,20 +8,47 @@ function FamousSection() {
   let [famousPeopleArray, setPeopleArray] = useState([]);
 
   // TODO: on load, call the fetchPeople() function
-
+  useEffect(() => {
+    //console.log('IN useEffect');
+    fetchPeople();
+  }, []);
+    
   const fetchPeople = () => {
     // TODO: fetch the list of people from the server
+    axios({
+      method: 'GET',
+      url: '/api/people'
+    })
+    .then((response) => {
+      setPeopleArray(response.data);
+    })
+    .catch((error) => {
+      console.error(error)
+    });
   }
 
   const addPerson = (evt) => {
     evt.preventDefault();
     console.log(`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`);
-    
+    const newPerson = {name: famousPersonName, role: famousPersonRole};
     // TODO: create POST request to add this new person to the database
-
+    axios({
+      method: 'POST',
+      url: '/api/people',
+      data: newPerson
+    })
+    .then((response) => {
+      fetchPeople()
+    })
+    .catch((error) => {
+      console.error(error);
+    })
     // HINT: the server is expecting a person object 
     //       with a `name` and a `role` property
-  
+
+    document.querySelector('#name-input').value = '';
+    document.querySelector('#role-input').value = '';
+
   }
 
     return (
@@ -37,9 +65,12 @@ function FamousSection() {
         </p>
         <ul>
           {/* TODO: Render the list of famous people */}
+          {famousPeopleArray.map(personData => {
+              return <li key={personData.id}> {personData.name}: Known For - {personData.role}</li>
+          })}
         </ul>
       </section>
     );
-}
 
+}
 export default FamousSection;
